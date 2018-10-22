d561949e0d939a0b0b26bce2eabb2dd533ecbac0
Oleg Ligay <ligayoleg@gmail.com>
2018-10-22 09:04:07 -0400

    project #1 screenshots

---
+++ Loops/LOOPS/Assets/Scripts/DebugDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugDrawer : MonoBehaviour {
+
+    public int numOfCubes;
+    public int spacing;
+
+    void OnDrawGizmos() //It is everything on the screen like camera icon or light icon
+    {//gives to designers a sense of what is going on in the scene
+
+        int otherinteger = 0;
+        int verticalinteger = 0;
+        Gizmos.color = Color.green;
+        for(int i = 0; i < numOfCubes; i++)
+        {
+            for (int j = 0; j < numOfCubes; j++)
+            {
+                Gizmos.DrawCube(new Vector3(otherinteger, verticalinteger, 0), Vector3.one);
+                otherinteger += spacing;
+            }
+            otherinteger = 0;
+            verticalinteger += spacing;
+        }
+
+        
+    }
+
+}
--- Project_1/Project#1/Assets/Scripts/ControlSprite.cs
+++ Project_1/Project#1/Assets/Scripts/ControlSprite.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlSprite : MonoBehaviour {
+public class ControlSprite : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector3 rotationVector;
+    // Use this for initialization}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(rotationVector * Time.deltaTime);
+    }
+
+
 }
