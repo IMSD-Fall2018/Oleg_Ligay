d7247b9a6ff54ba17d3d76f954665c3d2bc9e455
Oleg Ligay <ligayoleg@gmail.com>
2018-12-03 12:58:39 -0500

    organized the structure of repository

---
+++ Classes/class_09_27/class_10_17_18/Assets/scripts/FrameCatcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCatcher : MonoBehaviour {
+
+    public string folderName = "Screenshots";
+    public int frameRate = 30;
+    private int frameNum = 0;
+
+	// Use this for initialization
+	void Start () {
+        //1. Access a class "SYSTEM" that accesses root comands on computer.
+        //IO -> In/Out sublclass
+        System.IO.Directory.CreateDirectory(folderName);
+
+        Time.captureFramerate = frameRate; //<-- telling Unity to change capture Time.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string fileName = string.Format("{0}/frame{1:D04}.png",folderName,frameNum);
+        Debug.Log(fileName);
+        ScreenCapture.CaptureScreenshot(fileName);
+        frameNum++;
+	}
+}
+
+
+
+
+
+
